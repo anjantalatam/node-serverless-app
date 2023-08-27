38b47f3a25cf7fa3d73d8bb169009e31b3ff7914
Anjan Talatam <tspa2117@gmail.com>
2023-08-27 17:05:37 +0530

    connect aws lambda

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import serverless from 'serverless-http';
+
 const app = express();
 
 const port = 3000;
@@ -9,6 +11,14 @@ app.get('/health', (req, res) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hi from Serverless Express',
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+export const handler = serverless(app);
